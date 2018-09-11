6bc75111f62d58a36682599d9d17ccd474857195
popla <popla@komal>
2018-09-11 22:00:56 +0530

    modified

--- EAD/EAD_Assignment_11/EmployeePortalHibernate/src/main/java/com/metacube/training/employeeportalhibernate/dao/SkillDaoJDBCImpl.java
+++ EAD/EAD_Assignment_11/EmployeePortalHibernate/src/main/java/com/metacube/training/employeeportalhibernate/dao/SkillDaoJDBCImpl.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.metacube.training.employeeportalhibernate.mappers.SkillMapper;
+import com.metacube.training.employeeportalhibernate.model.Projects;
 import com.metacube.training.employeeportalhibernate.model.Skill;
 import com.metacube.training.employeeportalhibernate.query.Query;
 
@@ -19,18 +25,42 @@ public class SkillDaoJDBCImpl implements SkillDao{
 	private JdbcTemplate jdbcTemplate;
 	
 	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	/*@Autowired
     public SkillDaoJDBCImpl(DataSource dataSource) {
 		jdbcTemplate=new JdbcTemplate(dataSource);
-	}
+	}*/
 	
 	@Override
 	public List<Skill> getAll() {
-		return jdbcTemplate.query(Query.SQL_GET_ALL_SKILL,new SkillMapper());
+		Criteria criteria=sessionFactory.getCurrentSession().createCriteria(Projects.class);
+		List list=criteria.list();
+		return list;
+		//return jdbcTemplate.query(Query.SQL_GET_ALL_SKILL,new SkillMapper());
 	}
 
 	@Override
 	public boolean create(Skill skill) {
-		return jdbcTemplate.update(Query.SQL_INSERT_SKILL,skill.getName())>0;
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+
+		try{
+			tx = session.beginTransaction();
+			session.save(skill);
+			tx.commit();
+		}catch(HibernateException e){
+			if(tx!=null)
+				tx.rollback();
+			e.printStackTrace();
+			return false;
+		}finally{
+			session.close();
+		}
+
+		return true;
+		//return jdbcTemplate.update(Query.SQL_INSERT_SKILL,skill.getName())>0;
 	}
 
 	@Override
